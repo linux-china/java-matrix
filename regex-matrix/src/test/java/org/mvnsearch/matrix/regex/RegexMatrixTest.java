808253917369769f8cec20f307962ba9b6bdc8be
Jacky Chan <libing.chen@gmail.com>
2012-10-26 11:28:47 +0800

    regex matrix test

---
+++ regex-matrix/src/test/java/org/mvnsearch/matrix/regex/RegexMatrixTest.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.matrix.regex;
+
+import junit.framework.TestCase;
+
+/**
+ * regex matrix test
+ *
+ * @author linux_china
+ */
+public class RegexMatrixTest extends TestCase {
+
+    public void testSpike() throws Exception {
+
+    }
+
+    public void testMatrix() throws Exception {
+
+    }
+}
