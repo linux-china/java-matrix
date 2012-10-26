8d17236fcd705bc5741397db6e23b1768e2d400a
Jacky Chan <libing.chen@gmail.com>
2012-10-26 11:38:10 +0800

    regex matrix test

--- regex-matrix/src/test/java/org/mvnsearch/matrix/regex/RegexMatrixTest.java
+++ regex-matrix/src/test/java/org/mvnsearch/matrix/regex/RegexMatrixTest.java
@@ -2,18 +2,63 @@ package org.mvnsearch.matrix.regex;
 
 import junit.framework.TestCase;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * regex matrix test
  *
  * @author linux_china
  */
 public class RegexMatrixTest extends TestCase {
+    /**
+     * email regex pattern
+     */
+    private String emailPattern = "^[a-zA-Z0-9.!#$%&'*+/=?\\^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+    /**
+     * jdk regex pattern
+     */
+    private Pattern jdkRegexPattern = Pattern.compile(emailPattern);
 
+    /**
+     * spike test
+     *
+     * @throws Exception exception
+     */
     public void testSpike() throws Exception {
-
+        String email = "linux_china@hotmail.com";
+        System.out.println(jdk(email));
     }
 
+    /**
+     * matrix test
+     *
+     * @throws Exception exception
+     */
     public void testMatrix() throws Exception {
+        int count = 100000;
+        String email = "linux_china@hotmail.com";
+        //hot swap
+        for (int i = 0; i < 10000; i++) {
+            jdk(email);
+        }
+        System.out.println(count + " loop:");
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            jdk(email);
+        }
+        long end1 = System.currentTimeMillis();
+        System.out.println("jdk:" + (end1 - start));
+    }
 
+    /**
+     * jdk regex pattern
+     *
+     * @param text text
+     * @return result
+     */
+    private boolean jdk(String text) {
+        Matcher matcher = jdkRegexPattern.matcher(text);
+        return matcher.find();
     }
 }
