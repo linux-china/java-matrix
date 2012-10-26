40f9363dd251003062657466a7734421225c33e2
Jacky Chan <libing.chen@gmail.com>
2012-10-26 11:54:35 +0800

    add oro test

--- regex-matrix/src/test/java/org/mvnsearch/matrix/regex/RegexMatrixTest.java
+++ regex-matrix/src/test/java/org/mvnsearch/matrix/regex/RegexMatrixTest.java
@@ -1,6 +1,8 @@
 package org.mvnsearch.matrix.regex;
 
 import junit.framework.TestCase;
+import org.apache.oro.text.regex.Perl5Compiler;
+import org.apache.oro.text.regex.Perl5Matcher;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,6 +21,23 @@ public class RegexMatrixTest extends TestCase {
      * jdk regex pattern
      */
     private Pattern jdkRegexPattern = Pattern.compile(emailPattern);
+    /**
+     * oro perl5 pattern
+     */
+    org.apache.oro.text.regex.Pattern oroPerl5Pattern = null;
+
+    /**
+     * init logic
+     *
+     * @throws Exception exception
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        //oro
+        Perl5Compiler perl5Compiler = new Perl5Compiler();
+        oroPerl5Pattern = perl5Compiler.compile(emailPattern);
+    }
 
     /**
      * spike test
@@ -27,7 +46,7 @@ public class RegexMatrixTest extends TestCase {
      */
     public void testSpike() throws Exception {
         String email = "linux_china@hotmail.com";
-        System.out.println(jdk(email));
+        System.out.println(oro(email));
     }
 
     /**
@@ -41,6 +60,7 @@ public class RegexMatrixTest extends TestCase {
         //hot swap
         for (int i = 0; i < 10000; i++) {
             jdk(email);
+            oro(email);
         }
         System.out.println(count + " loop:");
         long start = System.currentTimeMillis();
@@ -49,16 +69,37 @@ public class RegexMatrixTest extends TestCase {
         }
         long end1 = System.currentTimeMillis();
         System.out.println("jdk:" + (end1 - start));
+        for (int i = 0; i < count; i++) {
+            oro(email);
+        }
+        long end2 = System.currentTimeMillis();
+        System.out.println("oro:" + (end2 - end1));
+
     }
 
     /**
-     * jdk regex pattern
+     * jdk regex
      *
      * @param text text
      * @return result
      */
     private boolean jdk(String text) {
         Matcher matcher = jdkRegexPattern.matcher(text);
-        return matcher.find();
+        boolean result = matcher.find();
+        matcher.group();
+        return result;
+    }
+
+    /**
+     * oro regex
+     *
+     * @param text text
+     * @return result
+     */
+    private boolean oro(String text) {
+        Perl5Matcher matcher = new Perl5Matcher();
+        boolean result = matcher.matches(text, oroPerl5Pattern);
+        matcher.getMatch().group(0);
+        return result;
     }
 }
